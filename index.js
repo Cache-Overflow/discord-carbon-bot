1b3b01ca8083e92acbe9a1d7a21b6c097c2cad88
eli-samuel <0.0elisamuel@gmail.com>
2020-05-16 19:24:35 -0400

    testing .commands

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 const token = "NzExMzQzNTIyNDUxODE2NDg5.XsBpRw.25YzHHuUBaJjpl7YD4swyJgpt08";
-
+const prefix= "."
 bot.login(token);
 
 // test function
@@ -10,10 +10,31 @@ bot.on("message", msg=>{
         return
     }
 
-    if (msg.content ==="Gabe") {
+    if (msg.content === "Gabe") {
         msg.channel.send("Yeah hes dirtyy");
     }
+    if (msg.content === "Lemme") {
+        msg.channel.send("He is the best!")
+    }
+    if (msg.content === "Carbon") {
+        msg.channel.send("DESTROY")
+    }
+    if (msg.content === "ayy") {
+        msg.channel.send("lmao");
+    }
+
+    if (msg.content.startsWith(".")) {
+        let args.msg.content.substring(prefix.length).split(" ");
+        var command = msg.content.substring(1)
+        msg.channel.send(command)
+        processCommand(command)
+    }
+    // if (msg.content.startsWith)
 })
+
+function processCommand(msg) {
+    console.log(msg);
+}
 
 
 bot.on('ready', () => {
