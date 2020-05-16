1eff6c0774832ca83cd4a5a208a1617b027482e8
davrine <daviddantelemme@gmail.com>
2020-05-16 18:08:27 -0400

    new token

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord=require("discord.js");
 const bot = new Discord.Client();
-const token ="NzExMzI3NjkxMTc3MDY2NTc2.XsBZow.Ydb98WZ-MMRwkEWoMZf0AItNdm8";
+const token = "NzExMzI3NjkxMTc3MDY2NTc2.XsBgkg.wE_nZqdRCKtt-WqHyajGgMacR54";
 
 bot.login(token);
 
