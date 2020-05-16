f7cb169cec27bb328024c6a8a1346a99bcef36a9
eli-samuel <0.0elisamuel@gmail.com>
2020-05-16 19:03:38 -0400

    added a ready message

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-const Discord=require("discord.js");
+const Discord = require("discord.js");
 const bot = new Discord.Client();
 const token = "NzExMzQzNTIyNDUxODE2NDg5.XsBpRw.25YzHHuUBaJjpl7YD4swyJgpt08";
 
 bot.login(token);
 
 // test function
-bot.on("greetings", msg=>{
-    if (msg.content ==="Gabe"){
-        msg.reply("Yeah hes dirtyy");
+bot.on("message", msg=>{
+    if (msg.content ==="Gabe") {
+        msg.channel.send("Yeah hes dirtyy");
     }
 })
 
-bot.on("ready", ()=>{
-    console.log("Ready to serve!");
-} )
+
+bot.on('ready', () => {
+    console.log("Logged in as " + bot.user.tag);
+
+    bot.channels.cache.get("711322092339200051").send("I am online!");
+})
