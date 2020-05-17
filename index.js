c5888207699e55e33aa659071f2ae538ec0eccca
eli-samuel <0.0elisamuel@gmail.com>
2020-05-16 22:03:12 -0400

    commented out things to test

--- index.js
+++ index.js
@@ -11,24 +11,24 @@ bot.on('ready', () => {
 });
 // Does an API call to Carbon Footprint calculator.
 // ***
-fetch("https://carbonfootprint1.p.rapidapi.com/CarbonFootprintFromPublicTransit?distance=5000&type=Taxi", {
-  "method": "GET",
-  "headers": {
-"x-rapidapi-host": "carbonfootprint1.p.rapidapi.com",
-"x-rapidapi-key": "b96c268c46msh6164d76db47a4fcp1bcf5ejsncad1a0d03078"
-}
-})
-.then(function (response) {
-      return response.json();
-    })
-.then(function (myJson) {
-      // var theDATA = JSON.parse('{ "name":"John", "age":30, "city":"New York"}');
-      console.log("The API Data is : "myJson.carbonEquivalent);
-      // console.log(theDATA);
-    })
-.catch(err => {
-  console.log(err);
-});
+// fetch("https://carbonfootprint1.p.rapidapi.com/CarbonFootprintFromPublicTransit?distance=5000&type=Taxi", {
+//   "method": "GET",
+//   "headers": {
+// "x-rapidapi-host": "carbonfootprint1.p.rapidapi.com",
+// "x-rapidapi-key": "b96c268c46msh6164d76db47a4fcp1bcf5ejsncad1a0d03078"
+// }
+// })
+// .then(function (response) {
+//       return response.json();
+//     })
+// .then(function (myJson) {
+//       // var theDATA = JSON.parse('{ "name":"John", "age":30, "city":"New York"}');
+//       console.log("The API Data is : "myJson.carbonEquivalent);
+//       // console.log(theDATA);
+//     })
+// .catch(err => {
+//   console.log(err);
+// });
 
 // test function
 bot.on("message", msg=>{
@@ -121,7 +121,7 @@ function david(material) {
     var a = material.toLowerCase();
     let mats = ["plastic", "paper", "cardboard", "glass", "tin", "aluminum", "steel"];
 
-    if(a == "plastic"){
+    if (a == "plastic") {
         msg.channel.send("Enter the type of plastic (1, 2, 3, 4, 5, 6) ex. .5");
         if (msg.content.startsWith(".")) {
             let pargs = msg.content.substring(prefix.length).split(" ");
@@ -142,6 +142,7 @@ function david(material) {
                 default:
                     return "not recyclable";
             }
+        }
     }
 
     if (mats.includes(a)) return "recyclable!";
